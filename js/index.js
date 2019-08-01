38cff6d120dde1d09da9bd6555fc8b5be53a569d
devin-bielejec <devin.bieleje@gmail.com>
2019-08-01 18:38:14 -0400

    Added src paths to images

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let cta = document.querySelector("#cta-img");
+cta.setAttribute('src', siteContent["cta"]["img-src"] )
+
+let middleImage = document.querySelector("#middle-img");
+middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
