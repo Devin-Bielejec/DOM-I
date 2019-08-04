d8f5069fc6817301b8c5ace9eae49e5e45768f0e
devin-bielejec <devin.bieleje@gmail.com>
2019-08-03 20:10:57 -0400

    Worked on stretch, almost there

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -6,7 +6,6 @@ const interval = window.setInterval( count, 10);
 //every 10ms, i want to 
 
 function count(){
-    console.log("hi");
     //add 10ms
     let msT = document.getElementById("msTens").textContent;
     msT === "-" ? msT = 0 : false;
@@ -19,7 +18,6 @@ function count(){
 
     let sT = document.getElementById("secondTens").textContent;
     sT === "-" ? sT = 0 : false;
-
     console.log(msT, msH, sO, sT);
     if (Number(msT) < 10) {
         msT++;
@@ -30,14 +28,13 @@ function count(){
         document.getElementById("msHundreds").textContent = msH;
     } else if (Number(sO) < 10) {
         document.getElementById("msHundreds").textContent = 0;
+        document.getElementById("msTens").textContent = 0;
         sO++;
         document.getElementById("secondOnes").textContent = sO;
-    } else if (Number(sT) < 10) {
-        document.getElementById("secondOnes").textContent = 0;
-        sT++;
-        document.getElementById("secondTens").textContent = sT;
+    } else if (Number(sT) < 1) {
+        document.getElementById("secondOnes").textContent = 1;
+        console.log("hi");
+        document.querySelector(".digit").style.color = "red";
+        }
+} 
 
-    }
-
-    
-}
